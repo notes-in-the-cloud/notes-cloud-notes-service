fd0f2e66c675b83ed9d3492dffca419892d6c350
ViktorMilanov05 <viktormilanov12@gmail.com>
2026-04-30 23:39:10 +0300

    change note object

---
+++ NotesService/NotesService/Models/CreateNoteDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace NotesService.Models
+{
+    public class CreateNoteDto
+    {
+        public int? Id { get; set; }
+
+        public int Id_User { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Color { get; set; }
+
+        public string Priority { get; set; }
+
+        public string UpdatedAt { get; set; }
+
+        public string CreatedAt { get; set; }
+    }
+}
