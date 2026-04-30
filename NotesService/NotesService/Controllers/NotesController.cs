c791aa448bc9f6315ed20dc0d6c64fa98ea57970
ViktorMilanov05 <viktormilanov12@gmail.com>
2026-04-30 17:10:30 +0300

    initial setup

---
+++ NotesService/NotesService/Controllers/NotesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesService.Models;
+using NotesService.Services.Interfaces;
+
+[ApiController]
+[Route("api/users/{userId}/notes")]
+public class NotesController : ControllerBase
+{
+    private readonly INotesService _service;
+
+    public NotesController(INotesService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll(Guid userId)
+    {
+        return Ok("hello");
+    }
+
+    [HttpGet("{noteId}")]
+    public IActionResult GetById(Guid userId, Guid noteId)
+    {
+        return Ok("hello");
+    }
+
+    [HttpPost]
+    public IActionResult Create(Guid userId, Note note)
+    {
+        return Ok("hello");
+    }
+
+    [HttpPut("{noteId}")]
+    public IActionResult Update(Guid userId, Guid noteId, UpdateNoteDto dto)
+    {
+        return Ok("hello");
+    }
+
+    [HttpDelete("{noteId}")]
+    public IActionResult Delete(Guid userId, Guid noteId)
+    {
+        return Ok("hello");
+    }
+}
---
+++ NotesService/NotesService/Services/Interfaces/INotesService.cs
@@ -0,0 +1,14 @@
+﻿using NotesService.Models;
+
+namespace NotesService.Services.Interfaces
+{
+    public interface INotesService
+    {
+        List<Note> GetAll(Guid userId);
+        Note GetById(Guid userId, Guid id);
+        Note Create(Note note);
+        void Delete(Guid userId, Guid id);
+
+        Note Update(Guid userId, Guid noteId, UpdateNoteDto updatedNote);
+    }
+}
