87b568ffb09bd4f7cbd132e2c0f17e19ea11e851
Mihael Mihaylov <mishoni04@abv.bg>
2026-05-09 16:17:04 +0300

    health and readyness

--- NotesService/NotesService/Program.cs
+++ NotesService/NotesService/Program.cs
@@ -4,10 +4,10 @@ using NotesService.Services.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
 builder.Services.AddScoped<INotesService, NoteService>();
 
 var app = builder.Build();
@@ -18,7 +18,19 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
+app.MapGet("/api/healthz", () => Results.Ok(new
+{
+    status = "UP",
+    service = "notes-service"
+}));
+
+app.MapGet("/api/readyz", () => Results.Ok(new
+{
+    status = "READY",
+    service = "notes-service"
+}));
 
 app.MapControllers();
 
